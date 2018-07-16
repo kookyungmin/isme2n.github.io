99e6bffce31bbd3247e75d9f69f0c5032e1eab43
gguri4549 <rudals4549@naver.com>
2018-07-16 11:42:53 +0900

    Update itscope.js

--- UI수업예제/itscope/itscope.js
+++ UI수업예제/itscope/itscope.js
@@ -1,11 +1,13 @@
 $(function(){
   $('.level1').click(function(){
-    $(this).find('.level2').slideDown(1000)
+     //현재 클릭한 것만 보이고 기존 메뉴는 slideUp
+    $('.level2).stop().slideUp(500) //stop은 반복 실행하면 그만두라는 
+     //현재의 level2만 slideDown
+    $(this).find('.level2').stop().slideDown(1000)
     //$(this).children('.level2').slideDown()
+    
+    
   })
-  
-  $('.level1').mouseleave(function(){
-     $(this).find('.level2').slideUp(1000)
-  })
+ 
   
 })
