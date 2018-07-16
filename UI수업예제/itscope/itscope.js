156e939012623898e74ea16e4bd341258b7576c2
gguri4549 <rudals4549@naver.com>
2018-07-16 11:35:10 +0900

    Update itscope.js

--- UI수업예제/itscope/itscope.js
+++ UI수업예제/itscope/itscope.js
@@ -1,10 +1,10 @@
 $(function(){
   $('.level1').click(function(){
-    $(this).$('.level2').slideDown(2000)
+    $(this).find('.level2').slideDown(2000)
   })
   
   $('.level1').mouseleave(function(){
-     $(this).$('.level2').slideUp(2000)
+     $(this).find('.level2').slideUp(2000)
   })
   
 })
