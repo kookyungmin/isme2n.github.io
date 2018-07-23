e64a58168de1b64752bf065021cc602fdfc7ff8b
gguri4549 <rudals4549@naver.com>
2018-07-23 10:32:57 +0900

    Create itscope3.js

---
+++ UI수업예제/itscope/itscope3.js
@@ -0,0 +1,14 @@
+$(function(){
+  $('.level1').click(function(){
+     //현재 클릭한 것만 보이고 기존 메뉴는 slideUp
+    
+    $('.level2').stop().slideUp(500) //stop은 반복 실행하면 그만두라는 
+     //현재의 level2만 slideDown
+    $(this).find('.level2').stop().slideDown(500)
+    //$(this).children('.level2').slideDown()
+    
+    
+  })
+ 
+  
+})
