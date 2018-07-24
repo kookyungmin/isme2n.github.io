f7d909feb645dbb9301b353b42e7fdb81779857e
gguri4549 <rudals4549@naver.com>
2018-07-24 12:41:15 +0900

    Add files via upload

--- UI수업예제/jQuery예제/u+알뜰모바일/uplus.js
+++ UI수업예제/jQuery예제/u+알뜰모바일/uplus.js
@@ -6,14 +6,30 @@ $(function(){
 		$('.content li').removeClass('on');
 		$(href).addClass('on');
 	})*/
-	/* index를 이용해 구현 */
-	$('.nav li').click(function(){
-		var idx=$(this).index() /* 인덱스를 가져옴 */
-		$('.content li').fadeOut()
-		$('.content li').eq(idx).fadeIn()
+	
+	var swiper = new Swiper('.swiper-container', {
+		autoplay: {
+			delaydelay: 2500,
+			disableOnInteraction: false,
+		 },
+	})
+	//swiper change 메서드	
+	swiper.on('slideChange', function () {
+		var activeIdx=swiper.activeIndex
+		//alert(activeIdx)	
+		$('.nav li').removeClass('on')
+		$('.nav li').eq(activeIdx).addClass('on')
 		$('.content2 li').fadeOut()
-		$('.content2 li').eq(idx).fadeIn()
+		$('.content2 li').eq(activeIdx).fadeIn()
+	});
+	
+	// 클릭했을 때
+	$('.nav li').click(function(){
+		var idx=$(this).index()
+		swiper.slideTo(idx,500,false) //슬라이드 움직임
 		$('.nav li').removeClass('on')
 		$(this).addClass('on')
+		$('.content2 li').fadeOut()
+		$('.content2 li').eq(idx).fadeIn()
 	})
 })
