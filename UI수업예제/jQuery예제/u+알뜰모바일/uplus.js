39e589330a2bdbe992ffbabab80a2f53293c9cd9
gguri4549 <rudals4549@naver.com>
2018-07-24 10:28:09 +0900

    Update uplus.js

--- UI수업예제/jQuery예제/u+알뜰모바일/uplus.js
+++ UI수업예제/jQuery예제/u+알뜰모바일/uplus.js
@@ -2,6 +2,7 @@ $(function(){
 	$('.nav li a').click(function(e){
 		e.preventDefault();
 		var href=this.getAttribute('href');
+		alert(this)
 		$('.content li').removeClass('on');
 		$(href).addClass('on');
 	})
