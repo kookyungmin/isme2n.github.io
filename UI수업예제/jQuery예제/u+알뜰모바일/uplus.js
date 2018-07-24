a7800c91aa149f480c3e2a9b3c137887235d74ac
gguri4549 <rudals4549@naver.com>
2018-07-24 10:55:42 +0900

    Add files via upload

--- UI수업예제/jQuery예제/u+알뜰모바일/uplus.js
+++ UI수업예제/jQuery예제/u+알뜰모바일/uplus.js
@@ -1,9 +1,19 @@
 $(function(){
-	$('.nav li a').click(function(e){
+	/* href를 가져와 구현 
+		$('.nav li a').click(function(e){
 		e.preventDefault();
 		var href=$(this).attr('href');
-		alert(href)
 		$('.content li').removeClass('on');
 		$(href).addClass('on');
+	})*/
+	/* index를 이용해 구현 */
+	$('.nav li').click(function(){
+		var idx=$(this).index() /* 인덱스를 가져옴 */
+		$('.content li').fadeOut()
+		$('.content li').eq(idx).fadeIn()
+		$('.content2 li').fadeOut()
+		$('.content2 li').eq(idx).fadeIn()
+		$('.nav li').removeClass('on')
+		$(this).addClass('on')
 	})
 })
