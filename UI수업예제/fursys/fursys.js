eb2fc7890069d8de2b00cb911eae3501ee7a6943
gguri4549 <rudals4549@naver.com>
2018-07-25 10:44:51 +0900

    Add files via upload

--- UI수업예제/fursys/fursys.js
+++ UI수업예제/fursys/fursys.js
@@ -1,9 +1,25 @@
 $(function(){
 	var swiper = new Swiper('.swiper-container', {
-      direction: 'vertical',
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
+		direction: 'vertical',
+		mousewheel: true,
+		autoplay: {
+			delaydelay: 2500,
+			disableOnInteraction: false,
+		},
     });
+	
+	//swiper change 메서드
+	swiper.on('slideChange', function () {
+		var activeIdx=swiper.activeIndex
+		//alert(activeIdx)	
+		$('.nav li').removeClass('on')
+		$('.nav li').eq(activeIdx).addClass('on')
+	});
+	
+	$('.nav li').mouseenter(function(){
+		var idx=$(this).index()
+		swiper.slideTo(idx,500,false) //슬라이드 움직임
+		$('.nav li').removeClass('on')
+		$(this).addClass('on')
+	})
 })
