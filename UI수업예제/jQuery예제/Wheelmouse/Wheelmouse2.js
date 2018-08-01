dc2e114aec7bf7bd9597495446dc08ebf462e932
kookyungmin <rudals4549@naver.com>
2018-08-02 00:40:21 +0900

    수정

--- UI수업예제/jQuery예제/Wheelmouse/Wheelmouse2.js
+++ UI수업예제/jQuery예제/Wheelmouse/Wheelmouse2.js
@@ -5,7 +5,6 @@ $(function(){
 		$(this).addClass('on');
 		
 		var href=$(this).attr('id'); //id 값 가져옴
-		
 		//가져온 id의 위치를 스크롤 left 값으로 변경(stop은 똑같은 것 클릭 계속해도 한번만 실행되게함)
 		$('html,body').stop().animate({scrollLeft:$(href).offset().left},500,'easeOutBounce')
 	})
